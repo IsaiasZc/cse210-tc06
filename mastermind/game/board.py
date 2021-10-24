14067a2f1c742b8af74c6ddc6ad62fe999cc0634
Luis Fernando <falconbladex1@gmail.com>
2021-10-23 19:10:25 -0500

    board commends

--- mastermind/game/board.py
+++ mastermind/game/board.py
@@ -3,6 +3,15 @@ import random
 class Board:
     
     def __init__(self):
+        """A designated playing surface. The responsibility of Board is to keep track of the pieces in play.
+    
+    Stereotype: 
+        Information Holder
+    Attributes:
+        _numbers (list): Stores a random a number between 1000-9999.
+        _chosen_number (Tuple): Two elements to do the comparison.
+        current (integer): Value of the user´s turn.
+    """
         self._numbers = None # Store the random number
         self._chosen_number = [["----","****"],["----","****"]] # Two elements to do the comparison
         self.current = -1 # The current user
@@ -18,6 +27,13 @@ class Board:
 
 
     def apply(self, move):
+        """Applies the given move to the playing surface. In this case, that 
+        means comparing the guessed numbers with the random numbers.
+        
+        Args:
+            self (Board): an instance of Board.
+            move (Move): The move to apply.
+        """
 
         # here we call the guessed number
         number = str(move.get_numbers())
@@ -39,20 +55,31 @@ class Board:
         player.append(guessed)            
 
 
-    def to_string(self):
+    def to_string(self, name):
+        """Converts the board data to its string representation.
+        Args:
+           self (Board): an instance of Board.
+        Returns:
+            string: A representation of the current board and players names.
+        """ 
         text = "\n--------------------"
         ## !IMPORTANT: I dont know how to add the players
         ## name but the information it's ok
         ## You could try to get the roster players in the  director
         ## and bring it here as a parameter, I'm not sure
         for number,guess in self._chosen_number:
-            text += (f"\nPlayer 1: {number},{guess}")
+            text += (f"\nPlayer {name}: {number}, {guess}")
 
 
         text += "\n--------------------"
         return text
 
     def _prepare(self):
+        """Sets up the board with a random number between 1000-9999.
+        
+        Args:
+            self (Board): an instance of Board.
+        """
         # To get the random number and transform it into to_string
         # It's necessary to store it as string for the comparisson
         number = random.randint(1000,9999)
